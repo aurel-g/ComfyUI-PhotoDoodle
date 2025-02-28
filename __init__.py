eb25090b50d291c25f9b398d5c570d8b9c656053
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-02-28 11:47:53 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+from .PhotoDoodle_nodes import PhotoDoodle
 
+NODE_CLASS_MAPPINGS = {
+    "PhotoDoodle Gen": PhotoDoodle,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "PhotoDoodle": "PhotoDoodle Gen",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
